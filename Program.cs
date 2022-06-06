59d9549890d1ab83935f23ad55774a91ec6f2f05
hiroi_junki <junki.hiroi@drecom.co.jp>
2022-06-06 19:46:25 +0900

    Add ArgumentParser

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var csvConverterArgument = new CsvConverterArgument();
+var argumentParser = new ArgumentParser();
+argumentParser.AddParamHook("--input", delegate (string arg) { csvConverterArgument.InputFilePath = arg; });
+argumentParser.AddParamHook("--output", delegate (string arg) { csvConverterArgument.OutputFilePath = arg; });
+argumentParser.Parse(args);
+
+Console.WriteLine(csvConverterArgument.InputFilePath);
+Console.WriteLine(csvConverterArgument.OutputFilePath);
+
+struct CsvConverterArgument
+{
+    public string InputFilePath { get; set; }
+    public string OutputFilePath { get; set; }
+}
+
+class ArgumentParser
+{
+    public void AddParamHook(string key, Action<string> action)
+    {
+        _paramHooks.Add(key, action);
+    }
+
+    private readonly Dictionary<string, Action<string>> _paramHooks = new();
+
+    public void Parse(string[] args)
+    {
+        for (var i = 0; i < args.Length; i++)
+        {
+            var arg = args[i];
+            if (_paramHooks.ContainsKey(arg))
+            {
+                var nextArg = args[i + 1];
+                _paramHooks[arg](nextArg);
+            }
+        }
+    }
+}
